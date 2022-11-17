9a9d9651fc9be6aec08716095893aebb4cb89385
PauliusTaraskevicius <fixas1029@gmail.com>
2022-11-17 22:40:10 +0200

    added loading state, missing image

--- components/movies/MovieCard.js
+++ components/movies/MovieCard.js
@@ -4,39 +4,53 @@ import Image from "next/image";
 import Meta from "../Meta";
 
 function MovieCard(props) {
-  const { id, title, overview, poster_path, vote_average, genre } = props.movie;
+  const { id, title, overview, poster_path, vote_average } = props.movie;
 
   return (
     <div>
       <Meta title="Most popular movies" description="Most popular movies" />
 
-      <div class="relative inline-block hover:scale-105 ease-in duration-300">
+      <div className="relative inline-block hover:scale-105 ease-in duration-300">
         <Link href={`/search/${id}`}>
-          <Image
-            className="block h-full w-full object-cover object-center cursor-pointer"
-            src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
-            width={300}
-            height={300}
-            alt={overview}
-          />
-        </Link>
+          {!poster_path ? (
+            <Image
+              className="block h-full w-full object-cover  cursor-pointer"
+              src="/images/no_image.png"
+              width={300}
+              height={600}
+              alt={overview}
+            />
 
-        <div
-          class="absolute bottom-0 right-0 w-12 h-12 bg-yellow-300 rounded-full"
-          style={{ right: "-10px", bottom: "-10px" }}
-        >
-          <div class="font-semibold text-xs flex justify-center items-center h-full">
-            TOP
+          ) : (
+            <Image
+              className="block h-full w-full object-cover object-center cursor-pointer"
+              src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+              width={300}
+              height={300}
+              alt={overview}
+            />
+          )}
+        </Link>
+        {vote_average >= 7 ? (
+          <div
+            className="absolute bottom-0 right-0 w-12 h-12 bg-yellow-300 rounded-full"
+            style={{ right: "-10px", bottom: "-10px" }}
+          >
+            <div className="font-semibold text-xs flex justify-center items-center h-full">
+              TOP
+            </div>
           </div>
-        </div>
+        ) : (
+          ""
+        )}
       </div>
       <a
         href="#"
-        class="block text-base font-semibold leading-tight text-white hover:text-blue-400 mt-8"
+        className="block text-base font-semibold leading-tight text-white hover:text-blue-400 mt-8"
       >
         {title}
       </a>
-      <div class="text-gray-400 mt-1">{vote_average}</div>
+      <div className="text-gray-400 mt-1">{vote_average}</div>
     </div>
   );
 }
