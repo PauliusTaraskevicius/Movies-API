54348ab0e6d2a10b6ab5b353af8595fc10054083
PauliusTaraskevicius <fixas1029@gmail.com>
2022-11-22 23:51:57 +0200

    deploymentv3

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -2,11 +2,11 @@ import Link from 'next/link'
 
 function Footer() {
   return (
-    <footer class="footer footer-center w-full p-4  text-white">
-      <div class="text-center">
+    <footer className="footer footer-center w-full p-4  text-white">
+      <div className="text-center">
         <p>
           Copyright © 2022 -
-          <Link class="font-semibold px-1" href="#">
+          <Link className="font-semibold px-1" href="#">
             Paulius Taraškevičius
           </Link>
         </p>
